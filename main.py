7cb8a8347adbfb4afc10600bde384cebf67a9054
SebastianSarzosa <131731807+SebastianSarzosa@users.noreply.github.com>
2024-01-18 23:09:37 -0500

    consultas con repositorio remoto con /docs

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+from fastapi import FastAPI
+
+#Creacion de una aplicacion FasTAPI:
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    return {'Nombre':'Sebastian  '}
+
+
+@app.get('/hola')
+def Hola_mundo():
+    return {'Hola mundo'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id : int, q: Union[str, None] = None):
+    return{ 'item_id' : item_id, 'q' : q}
+
+
+@app.get('/calculadora')
+def calcular(operando_1: float,operando_2:float):
+    return {'La suma de los dos numeros es' : operando_1+operando_2}
+
+
+
+
+
